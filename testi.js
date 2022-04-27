a5a0960c1ee08e2c5ee158f88f1f2854a72141a4
paikar2022 <n1kapa00@students.oamk.fi>
2022-04-27 09:24:14 +0300

    added cars and recipes tasks

--- testi.js
+++ testi.js
@@ -1,1 +1,53 @@
 /* function to be added here*/
+
+function test1 (tulosta){
+    if (tulosta) {
+        let printedText = "Testing";
+
+        document.getElementById("printHere").innerHTML = printedText;
+
+        return printedText;
+    }
+}
+let returnedText = test1 (true); 
+console.log(returnedText);
+
+//cars -task
+
+let car = {plateNumber: "OOP-001", currentSpeed: 120};
+
+document.getElementById("printHereCars").innerHTML = 
+"Car with plate number " + car.plateNumber + " is driving " + 
+car.currentSpeed +" km/h.";
+
+let car2 = {plateNumber: "SAA-002", currentSpeed: 80};
+
+document.getElementById("printHereCars").innerHTML += //remember += so it won't write over the previous car
+"<br>Car with plate number " + car2.plateNumber + " is driving " + 
+car2.currentSpeed +" km/h.";
+
+
+car2.currentSpeed = 100;//changed car2 speed
+
+document.getElementById("printHereCars").innerHTML += //remember += so it won't write over the previous car
+"<br>Car with plate number " + car2.plateNumber + " is driving " + 
+car2.currentSpeed +" km/h."; //printed out with new speed
+
+//recipe -tasks
+//created array variable and included recipes there
+//reseptiarrayssa itemit on olioita ja ingredients arrayssa itemit on strigejä
+let recipe = [
+    { name: "Tunapasta", cookingTime: 30, ingredients: ['tuna', 'pasta', 'olive oil']
+    },
+    { name: "Sallad", cookingTime: 15, ingredients: ['lettuce', 'tomatoe', 'cucumber']
+    },
+    { name: "Berrybowl", cookingTime: 5, ingredients: ['berries', 'fruits', 'yoghurt']
+    }    
+            ];
+let element = document.getElementById("printHereRecipes");
+//looped the receipes through and printed out
+
+recipe.forEach(recipe => {
+    element.innerHTML += recipe.name;
+})
+
